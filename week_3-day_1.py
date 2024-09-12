20a87dcb1b7fe2c9f256ce15c9aca05c768ce83e
Flores <asflores4@cps.k12.il.us>
2024-09-12 14:15:24 -0500

    variable naming

--- week_3-day_1.py
+++ week_3-day_1.py
@@ -111,13 +111,13 @@ print(f"{number}.{number1}.{number2}.{number3}.{number4}.{number5}.")
 # Correcting Invalid Names: Below are some invalid names. Correct them according to the rules:
 
 
-# 1st_name
+# first_name
 # last name
-# email@address
-# percent%
-# variable#name
-# O
-# list
+# email_address
+# percent
+# variable_name
+# zero
+# list_of_numbers
 # Creating Valid Names: Create valid names for the following descriptions:
 
 
@@ -138,10 +138,10 @@ print(f"{number}.{number1}.{number2}.{number3}.{number4}.{number5}.")
 # email_address
 # percentage
 # variable_name
-# 1_variable
-# email@address
-# percentage%
-# i
+# first_variable
+# email_address
+# percentage
+# italics
 
 
 
